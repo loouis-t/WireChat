f58e13853025664d7c11215fc62dd53c8009294e
Louis Travaux <louis.trvx@gmail.com>
2025-04-10 15:33:11 +0200

    [IMP] wireguard_mod: received messages loop + refactoring

--- src/wireguard_mod.rs
+++ src/wireguard_mod.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio_wireguard::{
     config::{Config, Interface, Peer},
@@ -6,77 +7,82 @@ use tokio_wireguard::{
     x25519::{PublicKey, StaticSecret},
 };
 use std::io::Error;
-use base64::decode;
+use base64::{engine::general_purpose::STANDARD, Engine as _};
 
 /// Convert a base64 string to a byte array of length 32
 fn bytes_from_base64(key: &str) -> [u8; 32] {
-    let bytes = decode(key).unwrap();
+    let bytes = STANDARD.decode(key).unwrap();
     let mut array = [0u8; 32];
     array.copy_from_slice(&bytes);
     array
 }
 
-/// Set up a WireGuard interface with the given parameters
-pub async fn setup_wireguard_interface(
-    interface_ip: &str,
-    peer_endpoint: &str,
-    allowed_ip: &str,
-    interface_private_key: &str,
-    peer_public_key: &str,
-) -> Result<(), Error> {
-
-    // println!("[*] Setting up WireGuard interface with IP: {interface_ip}, endpoint: {peer_endpoint}, allowed IP: {allowed_ip}");
-
-    let bytes = decode("").unwrap();
-    let mut array = [0u8; 32];
-    array.copy_from_slice(&bytes);
-    let public_cle = PublicKey::from(array);
-
-    let bytes = decode("").unwrap();
-    let mut array = [0u8; 32];
-    array.copy_from_slice(&bytes);
-    let private_cle = StaticSecret::from(array);
-
-    let configuration = Config {
-        interface: Interface {
-            private_key: private_cle,
-            address: "10.0.0.6/32".parse().unwrap(),
-            listen_port: None,
-            mtu: Some(1280),
-        },
-        peers: vec![Peer {
-            public_key: public_cle,
-            endpoint: Some("ip:port".parse().unwrap()),
-            allowed_ips: vec!["192.168.0.70/32".parse().unwrap()],
-            persistent_keepalive: Some(21),
-        }],
-    };
-
-    let interface = configuration.to_interface().await?;
-    println!("interface is closed ? : {:?}", interface.is_closed());
-
-    let mut stream = TcpStream::connect("192.168.0.70:8081", &interface).await?;
-    // println!("[*] Sending stream: {stream:?}");
-    stream.write_all(b"Bonjour").await?;
-
-    // read from the stream
-    let mut buffer = [0; 1024];
-    let n = stream.read(&mut buffer).await?;
-    // convert from bytes to string
-    let response = String::from_utf8_lossy(&buffer[..n]);
-    println!("Received: {}", response);
-
-    // tokio::time::sleep(std::time::Duration::from_secs(5)).await;
-    interface.close();
-
-    Ok(())
+pub struct WireGuardMultiChat {
+    interface: tokio_wireguard::interface::Interface,
+    peer_streams: HashMap<String, TcpStream>,
 }
 
-// /// Send a message to a peer over the WireGuard tunnel
-// pub async fn send_message_peer(ip: &str, message: &[u8], interface: &Interface) -> Result<(), Error> {
-//     println!("[*] WireGuard configuration: {interface:?}");
-//     println!("[*] Sending message to peer: {ip}");
-//     let mut stream = TcpStream::connect(format!("{ip}:8080").as_str(), interface).await?;
-//     println!("[*] Sending stream: {stream:?}");
-//     stream.write_all(message).await
-// }
+impl WireGuardMultiChat {
+    /// Set up a WireGuard interface with the given parameters
+    pub async fn new(
+        interface_ip: &str,
+        interface_private_key: &str,
+    ) -> Result<Self, Error> {
+        let configuration = Config {
+            interface: Interface {
+                private_key: StaticSecret::from(bytes_from_base64(interface_private_key)),
+                address: format!("{interface_ip}/32").parse().unwrap(),
+                listen_port: None,
+                mtu: Some(1280),
+            },
+            peers: vec![],
+        };
+
+        Ok(Self {
+            interface: configuration.to_interface().await?,
+            peer_streams: HashMap::new(),
+        })
+    }
+
+    /// Add a peer to the WireGuard interface
+    ///
+    /// TODO: Use IPv6 to merge ``endpoint`` and ``peer_ip``
+    pub async fn add_peer(
+        &mut self,
+        endpoint: &str,
+        peer_ip: &str,
+        public_key: &str
+    ) -> Result<(), Error> {
+        let peer = Peer {
+            public_key: PublicKey::from(bytes_from_base64(public_key)),
+            endpoint: Some(endpoint.parse().unwrap()),
+            allowed_ips: vec![format!("{peer_ip}/32").parse().unwrap()],
+            persistent_keepalive: Some(21),
+        };
+        self.interface.add_peer(peer).await?;
+
+        let stream = TcpStream::connect(format!("{peer_ip}:49369"), &self.interface).await?;
+        self.peer_streams.insert(public_key.to_string(), stream);
+        Ok(())
+    }
+
+    pub async fn send_message(&mut self, public_key: &str, message: &str) -> Result<(), Error> {
+        if let Some(mut peer) = self.peer_streams.get_mut(public_key) {
+            peer.write_all(message.as_bytes()).await
+        } else {
+            Err(Error::new(std::io::ErrorKind::NotFound, "Peer not found"))
+        }
+    }
+
+    pub async fn run_messaging_loop(&mut self) -> Result<(), Error> {
+        let mut buffer = [0; 1024];
+        loop {
+            for mut peer in self.peer_streams.values_mut() {
+                let n = peer.read(&mut buffer).await?;
+                if n != 0 {
+                    println!("Received: {}", String::from_utf8_lossy(&buffer[..n]));
+                }
+            }
+        }
+    }
+}
